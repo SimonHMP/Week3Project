8b59f30c30541d0fdc322593fcf9dc6725e4aac5
SimonHMP <simon.hempelman@gmail.com>
2019-11-18 17:55:36 +0100

    Added 3 components and trying to make horizontal scrolling work

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Boxes from "./components/boxes";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="HorizontalScroller">
+          <Boxes id="box1" />
+          <Boxes id="box2" />
+          <Boxes id="box3" />
+        </div>
       </header>
     </div>
   );
